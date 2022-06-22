ae7c51f2c8edbd476bceea345a4612f9e50d4fcb
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-06-22 10:29:53 +0900

    web socket setting

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import * as express from "express";
 const app = express();
 
@@ -5,8 +7,12 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
 });
