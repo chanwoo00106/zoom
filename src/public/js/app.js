ced549b34a019545312e56d565b0403b834d3af7
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-31 20:12:02 +0900

    Add room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,11 +3,24 @@ const socket = io();
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
 
+const room = document.querySelector("#room");
+room.hidden = true;
+
+let roomName = "";
+
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  room.querySelector("h3").innerText = `Room ${roomName}`;
+};
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input = form.querySelector("input");
 
   socket.emit("enter_room", { payload: input.value });
+  roomName = input.value;
+  showRoom();
 
   input.value = "";
 });
--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,12 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  socket.on("enter_room", (msg) => {
-    console.log(msg);
+  socket.onAny((event) => {
+    console.log(`Socket Event : ${event}`);
+  });
+
+  socket.on("enter_room", (roomName) => {
+    socket.join(roomName);
   });
 });
 
