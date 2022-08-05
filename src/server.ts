d0ee4f74a5e1cbbc5b37a66e500b66ffdb415c23
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-05 13:21:46 +0900

    Updaete clear

--- src/server.ts
+++ src/server.ts
@@ -12,63 +12,6 @@ app.get("/", (_, res) => res.render("home"));
 const server = http.createServer(app);
 const io = new Server(server);
 
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) publicRooms.push(key);
-  });
-
-  return publicRooms;
-};
-
-const countRoom = (roomName) => {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-};
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  io.sockets.emit("room_change", publicRooms());
-  socket.onAny((event) => {
-    console.log(`Socket Event : ${event}`);
-  });
-
-  socket.on("enter_room", (roomName) => {
-    socket.join(roomName.payload);
-    socket
-      .to(roomName.payload)
-      .emit("welcome", socket["nickname"], countRoom(roomName));
-
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket["nickname"], countRoom(room) - 1)
-    );
-  });
-
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room) => {
-    socket
-      .to(room.toString())
-      .emit("new_message", `${socket["nickname"]}: ${msg}`);
-  });
-
-  socket.on("nickname", (nickname) => {
-    socket["nickname"] = nickname;
-  });
-});
-
 server.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
 });
