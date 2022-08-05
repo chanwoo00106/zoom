098603fa340b48d8b0a306755c4860c2ace5163e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-05 13:55:02 +0900

    Add User video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,34 @@
 const socket = io();
+
+const myFace = document.querySelector("#myFace");
+const muteBtn = document.querySelector("#mute");
+const camerraBtn = document.querySelector("#camerra");
+
+let myStream;
+let muted = false;
+let camerraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: muted,
+      video: camerraOff,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+muteBtn.addEventListener("click", () => {
+  muted = !muted;
+  muteBtn.innerText = muted ? "Mute" : "Unmute";
+  getMedia();
+});
+camerraBtn.addEventListener("click", () => {
+  camerraOff = !camerraOff;
+  camerraBtn.innerText = camerraOff ? "Turn Camerra Off" : "Turn Camerra On";
+  getMedia();
+});
