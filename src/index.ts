0144f43e88b8c30202eaa7375b41716ef5650c8e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-20 23:56:52 +0900

    Add project setting

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (_, res) => {
+  res.render("index.ejs");
 });
 
 app.listen(8080, () => console.log("http://localhost:8080"));
